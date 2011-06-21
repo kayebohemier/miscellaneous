e2859882db0123d8f203ebc04619566b276614e8
Kaye Bohémier <kaye.bohemier@gmail.com>
2011-06-21 15:18:10 -0400

    New information in readme and a bit of code.

---
+++ madlib-story.py
@@ -0,0 +1,47 @@
+print "*******MAD LIB FUN*******"
+
+# It is vitally important that you not read the code. This is a
+# mad lib, so if you've read how it will be used, your experience
+# could be ruined. Please wait until after you run it to examine
+# how this was done and what could be made better!
+
+a = raw_input("Enter a type of opening [noun]:")
+b = raw_input("Enter a type of animal [noun, pl.]:")
+c = raw_input("Enter a verb ending in -ing:")
+d = raw_input("Enter a body part [noun, pl.]:")
+e = raw_input("Enter a noun [s.]:")
+f = raw_input("Enter an adjective:")
+g = raw_input("Enter a location [common noun]:")
+h = raw_input("Enter the singular version of the noun from b:")
+i = raw_input("Enter an abstract concept [noun]:")
+j = raw_input("Enter a noun:")
+k = raw_input("Enter a verb ending in -ing:")
+l = raw_input("Enter a verb in the infinitive [without 'to']:")
+m = raw_input("Enter a verb in the infinitive [without 'to']:")
+n = raw_input("Enter a verb in the present tense:")
+o = raw_input("Enter a verb ending in -ing that doesn't need an object:")
+p = raw_input("Enter a verb in the past perfect:")
+q = raw_input("Enter an alien or fantastical species or group:")
+r = raw_input("Enter a nonterrestrial location:")
+s = raw_input("Enter a verb in the infinitive [no 'to']:")
+t = raw_input("Enter an adjective:")
+u = raw_input("Enter a natural resource:")
+v = raw_input("Enter an industry:")
+
+print "When I looked out the", a, "that fateful morning, I saw \n a collection of", b, "milling about as though", c, "for something. \n One of them met my", d+", and I drew up the", e, "'Good morning,' I said,\n 'What has happened in this", f, "city?'\n The", h, "replied, 'Something has fallen from the", g+".'\n I had no memory or", i, "of anything like that happening before, so I\n dressed myself and went to join them outside. A rolling", j, "had lit up the", g , "\n behind the buildings, and I wondered if", k, "from the scene sounded like\n a better idea. Suddenly, the", j, "expanded to", l, "all of the buildings\n in front of us. I didn't know what to \n", m, "or do --- perhaps", n, "perhaps not --- but soon all of the", b, "started \n", o, "away. It turned out that we were being", p, "by", q, "from", r, "\n, who had decided to", s, "the Earth to exploit its", t, u, "and", v, "innovations."
+
+# When I looked out the window that fateful morning, I saw a collection
+# of small people milling about as though waiting for something. One of
+# them met my eyes, and I drew up the sash. "Good morning," I said, "What
+# has happened in this beautiful city?"
+# The person replied, "Something has fallen from the sky."
+# I had no memory or recollection of anything like that happening before,
+# so I dressed myself and went to join them outside. A rolling fire had lit
+# up the sky behind the buildings, and I wondered if retreating from the
+# scene sounded like a better idea.
+# Suddenly, the fire expanded to bake all of the buildings in front of us.
+# I didn't know what to say or do --- perhaps cry, perhaps not --- but
+# soon all of the small people started running away.
+# It turned out that we were being chased by Blancmanges from the Galaxy
+# of Andromeda, who had decided to invade the Earth to exploit its rich
+# minerals and baking equipment innovations.
